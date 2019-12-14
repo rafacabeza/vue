3696858c3959c2e201a52794c94643f51a82a6a4
Rafa Cabeza <rafacabeza@gmail.com>
2019-12-14 02:50:07 +0100

    descomponer tareas en dos componentes

--- js/app.js
+++ js/app.js
@@ -1,32 +1,23 @@
 //ver que data es una función
 //ver que el template tiene que tener un elemengo que lo englobe todo. Aquí div
-Vue.component('user', {
-    props: ['name', 'lastName'],
-    data: function() {
-        return {
-            app: {
-                name: 'Santiago Hernandez'
-            }
-        }
-    },
-    template: `<div>
-        <h1>{{ app.name }} </h1>
-        <h3>Nombre: {{ name }} {{ lastName }}</h3>
-        <input v-model="name">
-        <input v-model="app.name">
-    </div>`
-});
-
-var app = new Vue({
-    el: '#app',
-    data: {
-        newTask: "",
-        tasks: [
-            {text: "saludar", completed: true},
-            {text: "trabajar", completed: false},
-            {text: "despedir", completed: false}
-        ]
-    },
+Vue.component('tasks', {
+    template: `
+    <div>
+    <h1>Lista de tareas</h1>
+    <h3>Tareas completadas {{ completedTasks }}</h3>
+    <h3>Tareas no completadas {{ uncompletedTasks }}</h3>
+    <ul>
+        <li is="task" v-for="task in tasks" :task="task" :key="task.text"></li>
+        <li class="form-inline">
+            <input v-on:keyup.enter="addTask" v-model="newTask" type="text" class="form-control">
+            <button v-on:click="addTask()" class="btn btn-primary">+</button>
+        </li>
+        <li>
+            {{ reversedTask }}
+        </li>
+    </ul>
+    </div>    
+    `,
     methods: {
         addTask() {
             if (this.newTask) {
@@ -36,18 +27,55 @@ var app = new Vue({
                 });
                 this.newTask = '';
             }
-        },
-        toggle(task) {
-            task.completed = ! task.completed;
-        },
-        taskIcon(task) {
-            console.log("icon");
-            return ['glyphicon', task.completed ? 'glyphicon-check' : 'glyphicon-unchecked'];
+        }
+    },
+    data: function () {
+        return {
+            newTask: "",
+            tasks: [
+                {text: "saludar", completed: true},
+                {text: "trabajar", completed: false},
+                {text: "despedir", completed: false}
+            ]
         }
     },
     computed: {
         reversedTask: function() {
             return this.newTask.split('').reverse().join('');
+        }, 
+        completedTasks: function () {
+            return this.tasks.filter(function (task) {
+                return task.completed;
+            }).length;
+        }, 
+        uncompletedTasks: function () {
+            return this.tasks.filter(function (task) {
+                return !task.completed;
+            }).length;
         }
     }
+});
+
+
+Vue.component('task', {
+    props: ['task'],
+    template: `<li > 
+        <span v-text="task.text"></span> 
+        <span @click="toggle()" v-bind:class="taskIcon"></span> 
+        </li>`,
+        methods: {
+            toggle() {
+                this.task.completed = ! this.task.completed;
+            }
+        },
+        computed: {
+        taskIcon() {
+            console.log("icon");
+            return ['glyphicon', this.task.completed ? 'glyphicon-check' : 'glyphicon-unchecked'];
+        }
+    }
+});
+
+var app = new Vue({
+    el: '#app',
 });
